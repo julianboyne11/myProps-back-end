23f88effe359542625508d2d0f8c790241c73cbf
julianboyne <julianboyne11@gmail.com>
2022-10-21 14:26:09 -0400

    stub up tenant model

---
+++ models/tenant.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+  content: {type: String, required: true},
+},{
+  timestamps: true,
+})
+
+const tenantSchema = new Schema({
+  manager: { type: Schema.Types.ObjectId, ref: 'Profile' },
+  name: { type: String, required: true},
+  lease: {type: String, required: true},
+  current: Boolean,
+  listing: { type:  Schema.Types.ObjectId, ref: "Listing" },
+  comments: [commentSchema],
+  contact: Number,
+},{
+  timestamps: true,
+})
+
+const Tenant = mongoose.model('Tenant', tenantSchema)
+
+export { Tenant }
