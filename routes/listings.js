0235b1b5fca79554943bd31d36b8c907c82456ef
Tal <trimoni8@gmail.com>
2022-10-21 14:44:23 -0400

    listing routes

--- routes/listings.js
+++ routes/listings.js
@@ -4,6 +4,24 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
 
+router.use(decodeUserFromToken)
+
+router.get('/', checkAuth, listingsCtrl.index)
+
+router.get('/new', checkAuth, listingsCtrl.new)
+
+router.get('/:id', checkAuth, listingsCtrl.show)
+
+router.post('/', checkAuth, listingsCtrl.create)
+
+router.put('/:id', checkAuth, listingsCtrl.update)
+
+router.put('/:id/add-photo', checkAuth, listingsCtrl.addPhoto)
+
+router.delete('/:id', checkAuth, listingsCtrl.delete)
+
+
+
 
 
 export { router }
