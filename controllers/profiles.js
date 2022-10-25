44a4cd3293888f93fb44e4fa211dc076975be6ff
Tal <trimoni8@gmail.com>
2022-10-25 16:10:09 -0400

    photo add functionailty

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,6 +1,6 @@
 import { Profile } from "../models/profile.js";
 import { Listing } from "../models/listing.js";
-import { Tenant } from "../models/tenant.js"
+import { Tenant } from "../models/tenant.js";
 import { v2 as cloudinary } from "cloudinary";
 
 function index(req, res) {
@@ -14,7 +14,8 @@ function index(req, res) {
 
 function addPhoto(req, res) {
   const imageFile = req.files.photo.path;
-  Profile.findById(req.params.id).then((profile) => {
+  Profile.findById(req.params.id)
+  .then((profile) => {
     cloudinary.uploader
       .upload(imageFile, { tags: `${req.user.email}` })
       .then((image) => {
@@ -31,47 +32,43 @@ function addPhoto(req, res) {
 }
 
 function showMyListing(req, res) {
-  Profile.find({})
-  .then(profiles => {
+  Profile.find({}).then((profiles) => {
     Profile.findById(req.params.id)
-    .then(profile => {
-      Listing.find({ owner: profile._id})
-      .then(listing => {
-        res.status(200).json(listing)
+      .then((profile) => {
+        Listing.find({ owner: profile._id })
+          .then((listing) => {
+            res.status(200).json(listing);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
       })
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-  })
+  });
 }
 
 function showMyTenants(req, res) {
-  Profile.find({})
-  .then(profiles => {
+  Profile.find({}).then((profiles) => {
     Profile.findById(req.params.id)
-    .then(profile => {
-      Tenant.find({ manager: profile._id})
-      .then(tenants => {
-        res.status(200).json(tenants)
+      .then((profile) => {
+        Tenant.find({ manager: profile._id })
+          .then((tenants) => {
+            res.status(200).json(tenants);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
       })
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-  })
+  });
 }
 
-
-
-export { index, addPhoto, showMyListing, showMyTenants};
+export { index, addPhoto, showMyListing, showMyTenants };
